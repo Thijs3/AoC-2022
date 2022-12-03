9fbd37d30b4cf1f75d7db76600d1aa77c7803539
Thijs Visser <thijsvisser@bol.com>
2022-12-03 16:39:48 +0100

    Day 3 refactored

--- src/Day03.kt
+++ src/Day03.kt
@@ -3,25 +3,30 @@ fun main() {
         .zip(1..52) )
         .toMap()
 
-    fun String.splitInHalf() = substring(0, (length / 2)) to substring(length / 2)
+    fun Char.toPriority(): Int = itemTypePriorities.getOrDefault(this, 0)
 
-    fun Pair<String, String>.findCommonChar(): Char = first
+    fun String.splitInHalves() = substring(0, (length / 2)) to substring(length / 2)
+
+    fun Pair<String, String>.findCommonItem(): Char = first
         .first { char -> second.contains(char) }
 
-    fun List<String>.findBadge(): Char = first().first { char ->
-        sumOf { rucksack ->
-            rucksack.contains(char).compareTo(false)
-        } > 2
+    fun List<String>.findBadge(): Char = first()
+        .first { char ->
+            2 < sumOf { rucksack -> rucksack.contains(char).compareTo(false) }
     }
 
-    fun part1(input: List<String>): Int =
-        input.sumOf { items ->
-            itemTypePriorities.getOrDefault(items.splitInHalf().findCommonChar(), 0)
+    fun part1(input: List<String>): Int = input
+        .sumOf { items ->
+            items.splitInHalves()
+                .findCommonItem()
+                .toPriority()
         }
 
-    fun part2(input: List<String>): Int =
-        input.chunked(3).sumOf { group ->
-            itemTypePriorities.getOrDefault(group.findBadge(), 0)
+    fun part2(input: List<String>): Int = input
+        .chunked(3)
+        .sumOf { group ->
+            group.findBadge()
+                .toPriority()
         }
 
     val testInput = readInput("Day03_test")
