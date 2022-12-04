cdbed1901620f5f5a7775c6caf444a17420161b7
Thijs Visser <thijsvisser@bol.com>
2022-12-04 10:17:55 +0100

    cleanup

--- src/Utils.kt
+++ src/Utils.kt
@@ -15,13 +15,20 @@ fun readElvenCalories(name: String): List<List<Int>> = File("src", "$name.txt")
         }
     }
 
-fun readRockPaperScissors(name: String): List<Pair<String, String>> =
-    readInput(name).map { line -> Pair(line.split(" ")[0], line.split(" ")[1]) }
+fun readRockPaperScissors(name: String): List<Pair<String, String>> = readInput(name)
+    .map { line ->
+        line.split(" ")
+            .let { game ->
+                game.first() to game.last()
+            }
+    }
 
 fun readAssignments(name: String): List<Pair<IntRange,IntRange>> = readInput(name)
-    .map { line -> line.split(",") }.map { elves ->
-        Pair(elves.first().split("-").let { it.first().toInt()..it.last().toInt() },
-            elves.last().split("-").let {it.first().toInt()..it.last().toInt()})
+    .map { line ->
+        line.split(",")
+    }.map { elves ->
+        elves.first().split("-").let { it.first().toInt()..it.last().toInt() } to
+            elves.last().split("-").let {it.first().toInt()..it.last().toInt()}
     }
 
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
