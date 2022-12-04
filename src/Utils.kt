a06dc0dbf8a00566a33ffa638ddceff3b63cc316
Thijs Visser <thijsvisser@bol.com>
2022-12-04 10:13:26 +0100

    cleanup

--- src/Utils.kt
+++ src/Utils.kt
@@ -2,9 +2,6 @@ import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
 
-/**
- * Reads lines from the given input txt file.
- */
 fun readInput(name: String) = File("src", "$name.txt")
     .readLines()
 
@@ -27,9 +24,6 @@ fun readAssignments(name: String): List<Pair<IntRange,IntRange>> = readInput(nam
             elves.last().split("-").let {it.first().toInt()..it.last().toInt()})
     }
 
-/**
- * Converts string to md5 hash.
- */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
