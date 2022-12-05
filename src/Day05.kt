15b18a47155f0cf7e1836c65d86beb26214a4898
Thijs Visser <thijsvisser@bol.com>
2022-12-05 13:22:29 +0100

    Day 5

---
+++ src/Day05.kt
@@ -0,0 +1,43 @@
+data class CraneMove (
+    val amount: Int,
+    val from: Int,
+    val to: Int
+)
+
+class Cargo (
+    private val stacks: MutableList<String>,
+    private val moves: List<CraneMove>
+) {
+    fun stackTops(): String = stacks.map { it.last() }.joinToString("")
+    fun executeAllMoves9000() = moves.forEach { move ->
+        stacks[move.to] += stacks[move.from].takeLast(move.amount).reversed()
+        stacks[move.from] = stacks[move.from].dropLast(move.amount)
+    }
+
+    fun executeAllMoves9001() = moves.forEach { move ->
+        stacks[move.to] += stacks[move.from].takeLast(move.amount)
+        stacks[move.from] = stacks[move.from].dropLast(move.amount)
+    }
+}
+fun main() {
+
+    fun part1(cargo: Cargo): String {
+        cargo.executeAllMoves9000()
+        return cargo.stackTops()
+    }
+
+    fun part2(cargo: Cargo): String {
+        cargo.executeAllMoves9001()
+        return cargo.stackTops()
+    }
+
+    val testInput1 = readCargo("Day05_test")
+    val testInput2 = readCargo("Day05_test")
+    check(part1(testInput1) == "CMZ")
+    check(part2(testInput2) == "MCD")
+
+    val input1 = readCargo("Day05_input")
+    val input2 = readCargo("Day05_input")
+    println(part1(input1))
+    println(part2(input2))
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -31,6 +31,21 @@ fun readAssignments(name: String): List<Pair<IntRange,IntRange>> = readInput(nam
             elves.last().split("-").let {it.first().toInt()..it.last().toInt()}
     }
 
+fun readCargo(name: String): Cargo =  File("src", "$name.txt")
+    .readText()
+    .split("\n\n")
+    .map { it.split("\n") }
+    .let { (stacks, moves) ->
+        Cargo(stacks.toMutableList(), moves.toMoves())
+    }
+
+fun List<String>.toMoves(): List<CraneMove> = map { line ->
+    line.split(" ")
+        .let { (amount, from, to) ->
+            CraneMove(amount.toInt(), from.toInt() - 1, to.toInt() - 1)
+        }
+}
+
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
